9ced5eda184d6a4df6bd5b12d2ee13fbcaa03ab7
JavGalGas <145196986+JavGalGas@users.noreply.github.com>
2024-11-06 20:51:29 +0100

    commit "entrega prematura ejercicio"

---
+++ build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_appEjercicioPosicion_FILE D:/javgalgas/DI/Pruebas/Pruebas_Qt/EjercicioPosicion/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/appEjercicioPosicion.exe)
+set(__QT_DEPLOY_TARGET_appEjercicioPosicion_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_appEjercicioPosicion_RUNTIME_DLLS C:/Qt/6.7.2/mingw_64/bin/Qt6Quick.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6QmlModels.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Qml.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Network.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6OpenGL.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Core.dll)
